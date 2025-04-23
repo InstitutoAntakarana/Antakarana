af3381b0b392c7190550e47153f89a318c2e8b1b
InstitutoAntakarana <antakaranainstituto@gmail.com>
2025-04-22 22:20:42 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
+      <img src="/logo.png" alt="Instituto Antakarana" width="200" />
+      <h1>Instituto Antakarana</h1>
+      <p>Caminhos para a espiritualidade e o autoconhecimento</p>
+      <nav>
+        <a href="#">Blog</a> | <a href="#">Cursos</a> | <a href="#">Leituras</a>
+      </nav>
+    </div>
+  );
+}
